cb48fcbdec804c6dac26f5866428426e4f779a2f
jaysreeb <jaysreeb@gmail.com>
2026-05-21 15:35:33 +0530

    Updated pool connection

--- src/db.ts
+++ src/db.ts
@@ -1,11 +1,10 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  host:     process.env.DB_HOST,
-  port:     Number(process.env.DB_PORT),
-  database: process.env.DB_NAME,
-  user:     process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production' 
+    ? { rejectUnauthorized: false } 
+    : false
 });
 
 export default pool;
