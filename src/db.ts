108f5f8a231c758d730087a9995d229b3abd54f7
jaysreeb <jaysreeb@gmail.com>
2026-05-22 07:59:47 +0530

    Fix- Changing ssl to railways private url in db pool

--- src/db.ts
+++ src/db.ts
@@ -1,10 +1,7 @@
 import { Pool } from 'pg';
 
 const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production' 
-    ? { rejectUnauthorized: false } 
-    : false
+  connectionString: process.env.DATABASE_URL, 
+    ssl: false 
 });
-
 export default pool;
